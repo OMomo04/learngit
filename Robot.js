1322c1ace27ae683eb90ab084a709ab00d64f1b8
nanJavaScript <ouattara041@gmail.com>
2021-04-11 10:33:13 +0000

    Resolution du projet 1

---
+++ Robot.js
@@ -0,0 +1,8 @@
+class Robot{
+    //constructeur de mon robot
+    constructor(abscisse, ordonnee, face){
+        this.abscisse = abscisse;
+        this.ordonnee = ordonnee;
+        this.face = face;
+    }
+}
